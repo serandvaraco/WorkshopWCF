fbd0dfbb1acc315cb7066871857dbc737de5aa15
SERGIO ANDRES VARGAS <serandvaraco@outlook.com>
2018-07-11 10:54:30 -0500

    Added WCF Sercurity Client

---
+++ Demos/WCFSecurity/WCFSecurityClient/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace WCFSecurityClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsernameCreate = new System.Windows.Forms.TextBox();
+            this.txtPasswordCreate = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSendCreate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtConfirmPasswordCreate = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtPasswordNewChangePassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSendUpdatePassword = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPasswordChangePassword = new System.Windows.Forms.TextBox();
+            this.txtUsernameChangePassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPasswordConfirmChangePassword = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtRoleNameRole = new System.Windows.Forms.TextBox();
+            this.txtUsernameRole = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnSendRole = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtConfirmPasswordCreate);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnSendCreate);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtPasswordCreate);
+            this.groupBox1.Controls.Add(this.txtUsernameCreate);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(557, 140);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Crear usuario";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnSendRole);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.txtRoleNameRole);
+            this.groupBox3.Controls.Add(this.txtUsernameRole);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(13, 352);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(557, 112);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Asignar Roles";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre de usuario";
+            // 
+            // txtUsernameCreate
+            // 
+            this.txtUsernameCreate.Location = new System.Drawing.Point(9, 54);
+            this.txtUsernameCreate.Name = "txtUsernameCreate";
+            this.txtUsernameCreate.Size = new System.Drawing.Size(145, 29);
+            this.txtUsernameCreate.TabIndex = 1;
+            // 
+            // txtPasswordCreate
+            // 
+            this.txtPasswordCreate.Location = new System.Drawing.Point(188, 54);
+            this.txtPasswordCreate.Name = "txtPasswordCreate";
+            this.txtPasswordCreate.Size = new System.Drawing.Size(164, 29);
+            this.txtPasswordCreate.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contraseña";
+            // 
+            // btnSendCreate
+            // 
+            this.btnSendCreate.Location = new System.Drawing.Point(449, 89);
+            this.btnSendCreate.Name = "btnSendCreate";
+            this.btnSendCreate.Size = new System.Drawing.Size(102, 35);
+            this.btnSendCreate.TabIndex = 4;
+            this.btnSendCreate.Text = "Enviar";
+            this.btnSendCreate.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(383, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 21);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Confirmar";
+            // 
+            // txtConfirmPasswordCreate
+            // 
+            this.txtConfirmPasswordCreate.Location = new System.Drawing.Point(387, 54);
+            this.txtConfirmPasswordCreate.Name = "txtConfirmPasswordCreate";
+            this.txtConfirmPasswordCreate.Size = new System.Drawing.Size(164, 29);
+            this.txtConfirmPasswordCreate.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtPasswordConfirmChangePassword);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.txtPasswordNewChangePassword);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.btnSendUpdatePassword);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtPasswordChangePassword);
+            this.groupBox2.Controls.Add(this.txtUsernameChangePassword);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(13, 159);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(557, 187);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cambiar Contraseña";
+            // 
+            // txtPasswordNewChangePassword
+            // 
+            this.txtPasswordNewChangePassword.Location = new System.Drawing.Point(10, 127);
+            this.txtPasswordNewChangePassword.Name = "txtPasswordNewChangePassword";
+            this.txtPasswordNewChangePassword.Size = new System.Drawing.Size(164, 29);
+            this.txtPasswordNewChangePassword.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Contraseña Nueva";
+            // 
+            // btnSendUpdatePassword
+            // 
+            this.btnSendUpdatePassword.Location = new System.Drawing.Point(449, 146);
+            this.btnSendUpdatePassword.Name = "btnSendUpdatePassword";
+            this.btnSendUpdatePassword.Size = new System.Drawing.Size(102, 35);
+            this.btnSendUpdatePassword.TabIndex = 4;
+            this.btnSendUpdatePassword.Text = "Enviar";
+            this.btnSendUpdatePassword.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(199, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 21);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Contraseña Actual";
+            // 
+            // txtPasswordChangePassword
+            // 
+            this.txtPasswordChangePassword.Location = new System.Drawing.Point(203, 54);
+            this.txtPasswordChangePassword.Name = "txtPasswordChangePassword";
+            this.txtPasswordChangePassword.Size = new System.Drawing.Size(164, 29);
+            this.txtPasswordChangePassword.TabIndex = 2;
+            // 
+            // txtUsernameChangePassword
+            // 
+            this.txtUsernameChangePassword.Location = new System.Drawing.Point(9, 54);
+            this.txtUsernameChangePassword.Name = "txtUsernameChangePassword";
+            this.txtUsernameChangePassword.Size = new System.Drawing.Size(145, 29);
+            this.txtUsernameChangePassword.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(145, 21);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Nombre de usuario";
+            // 
+            // txtPasswordConfirmChangePassword
+            // 
+            this.txtPasswordConfirmChangePassword.Location = new System.Drawing.Point(203, 127);
+            this.txtPasswordConfirmChangePassword.Name = "txtPasswordConfirmChangePassword";
+            this.txtPasswordConfirmChangePassword.Size = new System.Drawing.Size(164, 29);
+            this.txtPasswordConfirmChangePassword.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(199, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(213, 21);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Confirmar Contraseña Nueva";
+            // 
+            // label8
+            // 
+            this.label8.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(196, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 21);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Rol";
+            // 
+            // txtRoleNameRole
+            // 
+            this.txtRoleNameRole.Location = new System.Drawing.Point(200, 49);
+            this.txtRoleNameRole.Name = "txtRoleNameRole";
+            this.txtRoleNameRole.Size = new System.Drawing.Size(164, 29);
+            this.txtRoleNameRole.TabIndex = 6;
+            // 
+            // txtUsernameRole
+            // 
+            this.txtUsernameRole.Location = new System.Drawing.Point(21, 49);
+            this.txtUsernameRole.Name = "txtUsernameRole";
+            this.txtUsernameRole.Size = new System.Drawing.Size(145, 29);
+            this.txtUsernameRole.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(18, 25);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(145, 21);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Nombre de usuario";
+            // 
+            // btnSendRole
+            // 
+            this.btnSendRole.Location = new System.Drawing.Point(449, 71);
+            this.btnSendRole.Name = "btnSendRole";
+            this.btnSendRole.Size = new System.Drawing.Size(102, 35);
+            this.btnSendRole.TabIndex = 9;
+            this.btnSendRole.Text = "Enviar";
+            this.btnSendRole.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 476);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bienvenido ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtConfirmPasswordCreate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSendCreate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPasswordCreate;
+        private System.Windows.Forms.TextBox txtUsernameCreate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSendRole;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtRoleNameRole;
+        private System.Windows.Forms.TextBox txtUsernameRole;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtPasswordConfirmChangePassword;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPasswordNewChangePassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSendUpdatePassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPasswordChangePassword;
+        private System.Windows.Forms.TextBox txtUsernameChangePassword;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Demos/WCFSecurity/WCFSecurityClient/LoginForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WCFSecurityClient
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(41, 152);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(291, 33);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(41, 228);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(291, 33);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre de usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(121, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contraseña";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(88, 293);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(172, 42);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Iniciar";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(88, 341);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(170, 37);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::WCFSecurityClient.Properties.Resources.bannerLogin;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(362, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 396);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "LoginForm";
+            this.Text = "Iniciar Sesión";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
